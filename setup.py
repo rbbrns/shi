0afa88b95c4f39a34a05529ff808fb3144a86f67
Rob Barnes <robertcbarnes@gmail.com>
2025-10-24 09:25:52 -0600

    Initial commit: Set up Python library shi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shi',
+    version='0.1.0',
+    packages=find_packages(),
+    author='Gemini',
+    author_email='',
+    description='A new Python library.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/your-username/shi',
+)
